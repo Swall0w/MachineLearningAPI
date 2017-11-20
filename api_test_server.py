9e244be82761abb6133ca74d2fbcdf154cfd265b
Swall0w <technext.jpn@gmail.com>
2017-11-20 23:46:10 +0900

    [update]: server.

--- api_test_server.py
+++ api_test_server.py
@@ -6,13 +6,100 @@ from chainercv import utils
 import os
 import socket
 from skimage import io
+import json
+import base64
 
 def arg():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu', type=int, default=-1)
     parser.add_argument('--pretrained_model', default='voc0712')
     parser.add_argument('--img', default='img.jpg')
+    parser.add_argument('--host', default="localhost", help='sergver address')
+    parser.add_argument('--port', '-p', default=4000, type=int, help='port')
     return  parser.parse_args()
+
+
+class Server(object):
+    def __init__(self, host='localhost', port=3000, verbose=False):
+        self.serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serversock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.serversock.bind((host, port))
+        self.serversock.listen(10)
+
+    def predict(self, data):
+        raise NotImplementedError
+
+    def run(self):
+        while True:
+            print('waiting for connections...')
+            clientsock, client_address = self.serversock.accept()
+            while True:
+                rcvmsg = clientsock.recv(4096 * 30)
+                if not rcvmsg:
+                    break
+
+                json_str = rcvmsg.decode('utf-8')
+                print(json_str)
+                print(len(json_str))
+                json_data = json.loads(json_str)
+                send_data = {}
+                status_data = {}
+                try:
+                    data = self.predict(json_data)
+                    status_data['code'] = str(200)
+                    status_data['details'] = ''
+
+                except:
+                    import traceback
+                    error = traceback.format_exc()
+                    data = {}
+                    data['None'] = []
+                    status_data['code'] = str(400)
+                    status_data['details'] = error
+                    print(error)
+
+                finally:
+                    send_data["status"] = status_data
+                    send_data["data"] = data
+                    converted_data = json.dumps(send_data, ensure_ascii=False)
+                    clientsock.sendall(converted_data.encode('utf-8'))
+            clientsock.close()
+
+class WeightServer(Server):
+    def __init__(self, host, port, model):
+        super().__init__(host, port)
+        self.infmodel = model
+
+    def predict(self, data):
+        # data must be in json format.
+        print(data)
+        print(data['frame'])
+        print(data['img'])
+        img = base64.b64decode(data['img'].encode('utf-8'))
+        img = cStringIO.StringIO(img)
+        img = io.imread(img)
+        img = img.transpose(2, 0, 1)
+
+        print('image shape: {}'.format(img.shape))
+        bboxes, labels, scores = self.infmodel.predict([img])
+
+        send_dict = {}
+        result_data = {}
+        object_list = []
+        result_data['frame'] = data['frame']
+        for num, bbox in enumerate(bboxes):
+            object_dict = {}
+            bndbox = {}
+            object_dict['name'] = voc_bbox_label_names[int(labels[num])]
+            object_dict['prob'] = str(float(scores[num]))
+            bndbox['ymin'] = str(int(bbox[0][0]))
+            bndbox['xmin'] = str(int(bbox[0][1]))
+            bndbox['ymax'] = str(int(bbox[0][2]))
+            bndbox['xmax'] = str(int(bbox[0][3]))
+            object_dict['bndbox'] = bndbox
+            object_list.append(object_dict)
+
+        result_data['object'] = object_list
 
 
 def main():
@@ -28,16 +115,21 @@ def main():
         model.to_gpu(args.gpu)
         chainer.cuda.get_device(args.gpu).use()
 
+    ws =  WeightServer(host=args.host, port=args.port, model=model)
+    ws.run()
+
+
 #    img = io.imread(args.img)
-    img = utils.read_image(args.img, color=True)
-    print(img)
-    print(img.shape)
-    bboxes, labels, scores = model.predict([img])
-    bbox, label, score = bboxes[0], labels[0], scores[0]
-    print(bboxes)
-    print(labels)
-    print(scores)
-    print(voc_bbox_label_names)
+#    img = img.transpose(2, 0, 1)
+#    print(img)
+#    print(img.shape)
+#    bboxes, labels, scores = model.predict([img])
+#    bbox, label, score = bboxes[0], labels[0], scores[0]
+#    print(bboxes)
+#    print(voc_bbox_label_names[int(label)])
+#    print(score)
+#    print(type(score))
+#    print(voc_bbox_label_names)
 
 
 if __name__ == '__main__':
