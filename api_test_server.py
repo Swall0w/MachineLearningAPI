59546705c47e1143bba3c41e77ccaba09f1111e1
Swall0w <technext.jpn@gmail.com>
2017-11-20 21:28:52 +0900

    [update]: print.

--- api_test_server.py
+++ api_test_server.py
@@ -5,13 +5,13 @@ from chainercv.links import SSD300
 from chainercv import utils
 import os
 import socket
+from skimage import io
 
 def arg():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu', type=int, default=-1)
     parser.add_argument('--pretrained_model', default='voc0712')
-    parser.add_argument('src_dir')
-    parser.add_argument('dst_dir')
+    parser.add_argument('--img', default='img.jpg')
     return  parser.parse_args()
 
 
@@ -27,6 +27,17 @@ def main():
     if args.gpu >= 0:
         model.to_gpu(args.gpu)
         chainer.cuda.get_device(args.gpu).use()
+
+#    img = io.imread(args.img)
+    img = utils.read_image(args.img, color=True)
+    print(img)
+    print(img.shape)
+    bboxes, labels, scores = model.predict([img])
+    bbox, label, score = bboxes[0], labels[0], scores[0]
+    print(bboxes)
+    print(labels)
+    print(scores)
+    print(voc_bbox_label_names)
 
 
 if __name__ == '__main__':
