be85d48dce32ed5c375683f00d9f25951802471e
Swall0w <technext.jpn@gmail.com>
2017-11-23 19:25:11 +0900

    [update]: detection detail.

--- echo_server.py
+++ echo_server.py
@@ -24,21 +24,47 @@ def main():
             if not rcvmsg:
                 break
 
+            ret = {}
+            status = {}
+            data = {}
+
             try:
                 json_data = rcvmsg.decode('utf-8')
                 print(json_data)
                 json_data = json.loads(json_data)
-                ret = {}
-                ret['frame'] = str(int(json_data['frame']) + 1)
-                ret['img'] = 'fuga' * int(json_data['frame'])
+#                ret['frame'] = str(int(json_data['frame']) + 1)
+#                ret['img'] = 'fuga' * int(json_data['frame'])
+                data['frame'] = json_data['frame']
+                objct_all = []
+                obj1 = {}
+                obj1['name'] = 'dump'
+                obj1['prob'] = str(0.8533421431)
+                bndbox={"xmin": str(10), "xmax":str(100),
+                        "ymin": str(15), "ymax":str(200)}
+                obj1['bndbox'] = bndbox
+                objct_all.append(obj1)
+                obj2 = {}
+                obj2['name'] = 'excavator'
+                obj2['prob'] = str(0.2501)
+                bndbox={"xmin": str(100), "xmax":str(150),
+                        "ymin": str(30), "ymax":str(50)}
+                obj2['bndbox'] = bndbox
+                objct_all.append(obj2)
+                data['object'] = objct_all
+
+                status['code'] = str(200)
+                status['details'] = ""
+                
             except:
                 import traceback
                 error = traceback.format_exc()
                 print(error)
-                ret = {}
-                ret['frame'] = str(0)
-                ret['img'] = error
+                status['code'] = str(400)
+                status['details'] = error
+
             finally:
+                ret['status'] = status
+                ret['data'] = data
                 ret = json.dumps(ret).encode('utf-8')
                 clientsock.sendall(ret)
                 print('done')
