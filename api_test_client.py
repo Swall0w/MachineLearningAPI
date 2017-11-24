62c26f96b28d08c951619f403adada1680735898
Swall0w <technext.jpn@gmail.com>
2017-11-24 09:23:43 +0900

    [update]: it tells max iter.

--- api_test_client.py
+++ api_test_client.py
@@ -10,6 +10,7 @@ import json
 import base64
 from io import StringIO, BytesIO
 import zlib
+import math
 
 
 def arg():
@@ -19,6 +20,7 @@ def arg():
     parser.add_argument('--img', nargs=1, type=str, help='image files')
     parser.add_argument('--verbose', action='store_true', default=False,
                         help='debug mode if this flag is set (default: False)')
+    parser.add_argument('--length', '-l', default=4096, type=int, help='packet length')
     return parser.parse_args()
 
 
@@ -30,7 +32,6 @@ def main():
     senddict['frame'] = str(5)
     with open(imgfile, 'rb') as image_file:
         encoded_string = base64.b64encode(image_file.read())
-        print(type(encoded_string))
 
     senddict['img'] = encoded_string.decode('utf-8')
 
@@ -43,8 +44,11 @@ def main():
     client.connect((args.host, args.port))
     img_json = img_json.encode('utf-8')
 
+    num = math.ceil(len(img_json)/ args.length) + 1
+
+    client.send(str(num).encode('utf-8'))
     status = client.sendall(img_json)
-    print('byte: {},  status: {}'.format(len(img_json), status))
+    print('num: {}, byte: {},  status: {}'.format(num, len(img_json), status))
 
     response = client.recv(4096).decode('utf-8')
     print(response)
